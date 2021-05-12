9f5ceaad0f189bbb0ee7d1928fd964521166d266
SolomidHero <dim.sad23@gmail.com>
2021-05-13 00:04:13 +0300

    test pathes fixed

--- utils/hparams.py
+++ utils/hparams.py
@@ -3,6 +3,6 @@ from pathlib import Path
 import __main__
 
 
-root_dir = Path(__main__.__file__).parent
+root_dir = Path(__file__).parent.parent.resolve()
 cfg = OmegaConf.load(root_dir / 'config.yaml')
 cfg.root_dir = str(root_dir.resolve())
